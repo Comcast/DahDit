62cb81646cf3538d01c18cdee82ca6fedd172d71
Schrag, Eric <Eric_Schrag@Comcast.com>
2017-10-24 11:27:45 -0400

    Initial commit

---
+++ library/src/main/java/com/xfinity/dahdit/DashedLine.kt
@@ -0,0 +1,106 @@
+package com.xfinity.dahdit
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.DashPathEffect
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+
+// TODO: Restore defStyleRes when we can understand constructors and init blocks.
+class DashedLine
+@JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    : View(context, attrs, defStyleAttr) {
+
+    var dashHeight: Float
+    var dashLength: Float
+    var minimumDashGap: Float
+    var orientation = Orientation.HORIZONTAL
+
+    val path: Path = Path()
+    val paint: Paint = Paint()
+
+    init {
+        val metrics = resources.displayMetrics
+        val twoDpDefault = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2f, metrics)
+        val defaultBlack = Color.argb(255, 0, 0, 0)
+
+        if (attrs != null) {
+            val typedArray = context?.theme?.obtainStyledAttributes(attrs, R.styleable.DashedLine, defStyleAttr, 0)
+            dashHeight = typedArray?.getDimension(R.styleable.DashedLine_dashHeight, twoDpDefault) ?: twoDpDefault
+            dashLength = typedArray?.getDimension(R.styleable.DashedLine_dashLength, twoDpDefault) ?: twoDpDefault
+            minimumDashGap = typedArray?.getDimension(R.styleable.DashedLine_minimumDashGap, twoDpDefault) ?: twoDpDefault
+            paint.color = typedArray?.getColor(R.styleable.DashedLine_dashColor, defaultBlack) ?: defaultBlack
+            val orientationOrdinal = typedArray?.getInt(R.styleable.DashedLine_orientation, DottedLine.Orientation.HORIZONTAL.ordinal)
+                    ?: DottedLine.Orientation.HORIZONTAL.ordinal
+            if (orientationOrdinal == DottedLine.Orientation.VERTICAL.ordinal) {
+                orientation = Orientation.VERTICAL
+            } else {
+                orientation = Orientation.HORIZONTAL
+            }
+            typedArray?.recycle()
+        } else {
+            dashHeight = twoDpDefault;
+            dashLength = twoDpDefault;
+            minimumDashGap = twoDpDefault;
+            paint.color = defaultBlack
+        }
+
+        paint.strokeWidth = dashHeight
+        paint.style = Paint.Style.STROKE
+
+
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        if (orientation == Orientation.HORIZONTAL) {
+            val widthNeeded = paddingLeft + paddingRight + suggestedMinimumWidth
+            val width = resolveSize(widthNeeded, widthMeasureSpec)
+
+            val heightNeeded = paddingTop + paddingBottom + dashHeight
+            val height = resolveSize(heightNeeded.toInt(), heightMeasureSpec)
+
+            setMeasuredDimension(width, height)
+        } else {
+            val widthNeeded = paddingLeft + paddingRight + dashHeight
+            val width = resolveSize(widthNeeded.toInt(), widthMeasureSpec)
+
+            val heightNeeded = paddingTop + paddingBottom + suggestedMinimumHeight
+            val height = resolveSize(heightNeeded, heightMeasureSpec)
+
+            setMeasuredDimension(width, height)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        path.reset()
+        path.moveTo(paddingLeft.toFloat(), paddingTop.toFloat())
+        if (orientation == Orientation.HORIZONTAL) {
+            val w = canvas.width - paddingLeft - paddingRight
+            val d = dashLength
+            val m = minimumDashGap
+            val c: Int = Math.floor(((w - d) / (d + m)).toDouble()).toInt()
+            val g: Float = (w - (d * (c + 1))) / c
+            path.lineTo((canvas.width - paddingLeft - paddingRight).toFloat(),
+                    paddingTop.toFloat())
+            paint.pathEffect = DashPathEffect(floatArrayOf(d, g), 0f)
+        } else {
+            val h = (canvas.height - paddingTop - paddingBottom).toFloat()
+            val d = dashLength
+            val m = minimumDashGap
+            val c: Int = Math.floor(((h - d) / (d + m)).toDouble()).toInt()
+            val g: Float = (h - (d * (c + 1))) / c
+            paint.pathEffect = DashPathEffect(floatArrayOf(d, g), 0f)
+            path.lineTo(paddingLeft.toFloat(), h)
+        }
+        canvas.drawPath(path, paint)
+    }
+
+    enum class Orientation {
+        HORIZONTAL, VERTICAL
+    }
+
+}
---
+++ library/src/main/java/com/xfinity/dahdit/DottedLine.kt
@@ -0,0 +1,102 @@
+package com.xfinity.dahdit;
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+
+// TODO: Restore defStyleRes when we can understand constructors and init blocks.
+class DottedLine
+@JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    : View(context, attrs, defStyleAttr) {
+
+    var dotRadius: Float
+    var minimumDotGap: Float
+    var orientation = Orientation.HORIZONTAL
+
+    val paint: Paint = Paint()
+
+    init {
+        val metrics = resources.displayMetrics
+        val twoDpDefault = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2f, metrics)
+        val defaultBlack = Color.argb(255, 0, 0, 0)
+
+        if (attrs != null) {
+            val typedArray = context?.theme?.obtainStyledAttributes(attrs, R.styleable.DottedLine, defStyleAttr, 0)
+            dotRadius = typedArray?.getDimension(R.styleable.DottedLine_dotRadius, twoDpDefault) ?: twoDpDefault
+            minimumDotGap = typedArray?.getDimension(R.styleable.DottedLine_minimumDotGap, twoDpDefault) ?: twoDpDefault
+            paint.color = typedArray?.getColor(R.styleable.DottedLine_dotColor, defaultBlack) ?: defaultBlack
+            val orientationOrdinal = typedArray?.getInt(R.styleable.DottedLine_orientation, Orientation.HORIZONTAL.ordinal)
+                    ?: Orientation.HORIZONTAL.ordinal
+            if (orientationOrdinal == Orientation.VERTICAL.ordinal) {
+                orientation = Orientation.VERTICAL
+            } else {
+                orientation = Orientation.HORIZONTAL
+            }
+            typedArray?.recycle()
+        } else {
+            dotRadius = twoDpDefault
+            minimumDotGap = twoDpDefault
+            paint.color = defaultBlack
+        }
+
+        paint.style = Paint.Style.FILL
+        paint.flags = Paint.ANTI_ALIAS_FLAG
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        if (orientation == Orientation.HORIZONTAL) {
+            val widthNeeded = paddingLeft + paddingRight + suggestedMinimumWidth
+            val width = resolveSize(widthNeeded, widthMeasureSpec)
+
+            val heightNeeded = paddingTop + paddingBottom + 2 * dotRadius
+            val height = resolveSize(heightNeeded.toInt(), heightMeasureSpec)
+
+            setMeasuredDimension(width, height)
+        } else {
+            val widthNeeded = paddingLeft + paddingRight + 2 * dotRadius
+            val width = resolveSize(widthNeeded.toInt(), widthMeasureSpec)
+
+            val heightNeeded = paddingTop + paddingBottom + suggestedMinimumHeight
+            val height = resolveSize(heightNeeded, heightMeasureSpec)
+
+            setMeasuredDimension(width, height)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if (orientation == Orientation.HORIZONTAL) {
+            val w = canvas.width - paddingLeft - paddingRight
+            val d = 2 * dotRadius
+            val m = minimumDotGap
+            val c: Int = Math.floor(((w - d) / (d + m)).toDouble()).toInt()
+            val g: Float = (w - (d * (c + 1))) / c
+            for (i in 0..c) {
+                canvas.drawCircle(paddingLeft + dotRadius + i * (d + g),
+                        paddingTop + dotRadius,
+                        dotRadius,
+                        paint)
+            }
+        } else {
+            val h = (canvas.height - paddingTop - paddingBottom).toFloat()
+            val d = 2 * dotRadius
+            val m = minimumDotGap
+            val c: Int = Math.floor(((h - d) / (d + m)).toDouble()).toInt()
+            val g: Float = (h - (d * (c + 1))) / c
+            for (i in 0..c) {
+                canvas.drawCircle(paddingLeft + dotRadius,
+                        paddingTop + dotRadius + i * (d + g),
+                        dotRadius,
+                        paint)
+            }
+        }
+
+    }
+
+    enum class Orientation {
+        HORIZONTAL, VERTICAL
+    }
+}
